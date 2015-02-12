fe7196defccd6fcdff42ecda5ae5f21e488249ca
John Blakeney <blakeney@alum.mit.edu>
2015-02-12 16:01:38 -0500

    Create vimsetup.sh

---
+++ util/vimsetup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Set up Vim
+
+# Swap Directory
+mkdir -p ~/.vim/swapfiles
+
+# VIMRC File
+curl -LSso ~/.vimrc https://raw.githubusercontent.com/blakeney/dotfiles/master/.vimrc
+
+# Pathogen (VIM package manager)
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Plugins
+dir = `pwd`
+cd ~/.vim/bundle
+git clone https://github.com/JalaiAmitahl/maven-compiler.vim 
+git clone https://github.com/fholgado/minibufexpl.vim 
+git clone https://github.com/scrooloose/nerdtree 
+git clone https://github.com/scrooloose/syntastic 
+git clone https://github.com/tpope/vim-fugitive 
+git clone https://github.com/derekwyatt/vim-scala 
+git clone https://github.com/cespare/vjde 
+cd $dir
